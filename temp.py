74e80c2e929746a5157a7c0b41be659726040a3f
Alexander Donskoy <donskoyaleksander@gmail.com>
2023-11-27 10:12:28 +0300

    add working version without classes

--- temp.py
+++ temp.py
@@ -1,23 +1,23 @@
 from math import sqrt
 
-
 message = ('Добро пожаловать в самую лучшую программу для вычисления '
            'квадратного корня из заданного числа')
 print(message)
 
 
-def CalculateSquareRoot(number):
+def calculate_square_root(number):
     """Вычисляет квадратный корень."""
     return sqrt(number)
 
 
 def calc(your_number):
-    """Docstring"""
+    """Docstring."""
     if your_number <= 0:
         return 0
-    feature = CalculateSquareRoot(your_number)
+    root = calculate_square_root(your_number)
     print(f'Мы вычислили квадратный корень из введённого '
-          f'вами числа. Это будет: {feature}')
+          f'вами числа. Это будет: {root}')
+    return root
 
 
 print(message)
